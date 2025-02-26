03d78ca49d338db8733609f941a032352bfc5ff3
Anel Canto <contact@anelcanto.com>
2025-02-26 15:17:40 -0700

    Add resume app

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import ResumeDisplay from '@/components/ResumeDisplay';
+
+const ResumePage: React.FC = () => {
+    return (
+        <div>
+            <ResumeDisplay />
+        </div>
+    );
+};
+
+export default ResumePage;
