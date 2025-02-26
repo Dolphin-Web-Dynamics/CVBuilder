2476fd49b2ccaff46f4b7bafdef244e73ecc37ea
Anel Canto <ac@anelcanto.com>
2025-02-26 01:02:18 -0700

    refactor code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@
 
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
-// import type { Schema } from "@/amplify/data/resource";
 import type { Schema } from "amplify/data/resource";
-import "./../app/app.css";
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+import outputs from "amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
